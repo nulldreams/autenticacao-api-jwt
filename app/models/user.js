7d251e1d66f6369b6488f119de5bda1263ac44db
Igor Souza Martins <nulldreams@gmail.com>
2016-11-24 11:55:23 -0200

    Add files via upload

--- app/models/user.js
+++ app/models/user.js
@@ -1,9 +1,9 @@
-// Vamos adicionar nosso pacote do mongoose 
-var mongoose 	  = require('mongoose')
-// e criar um objeto com schema do nosso usuário que será salvo no banco.
+// Vamos adicionar nosso pacote do mongoose e criar um objeto com schema do nosso usuário que será salvo no banco.
+var mongoose = require('mongoose')
+
 var usuarioSchema = new mongoose.Schema({
-		name     : String,
-		password : String
+  name: String,
+  password: String
 })
 
 // Agora aqui vamos devolver o Schema do usuário para quer acessar este arquivo
